1b2e336ddbed5ada1eb92225c2d924a52ee19bd6
Ademario Santana <44641613+AdeLuigi@users.noreply.github.com>
2022-03-07 22:53:36 -0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -23,7 +23,7 @@ CREATE TABLE `candidatos` (
   `partido` varchar(200) DEFAULT NULL,
   `foto` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`idcandidatos`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
